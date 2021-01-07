a433805aa4d545ea4215e9f2c483409978f63301
Iturea <daniel.penrod@microsoft.com>
2021-01-07 08:46:31 -0500

    updates to baseline

--- baseline.cmd
+++ baseline.cmd
@@ -27,30 +27,47 @@ ipconfig | findstr IPv6
 ECHO ============================
 ECHO UPDATES
 ECHO ============================
-choco upgrade 7zip -y
+:: Developer Updates
 choco upgrade nodejs -y
 choco upgrade bower -y
 choco upgrade yarn -y
 choco upgrade git -y
 choco upgrade gh -y
-choco upgrade visualstudio2019enterprise -y
-choco upgrade vscode -y
-choco upgrade dotnetcore-sdk -y
-choco upgrade azure-cli -y
-choco upgrade azure-functions-core-tools -y
-choco upgrade rust -y
-choco upgrade sql-server-express -y
-choco upgrade azure-data-studio -y
-choco upgrade skype -y
-choco upgrade notepadplusplus -y
-choco upgrade microsoft-teams -y
-choco upgrade obs-studio -y
 choco upgrade microsoft-edge -y
 choco upgrade googlechrome -y
 choco upgrade firefox -y
+choco upgrade visualstudio2019enterprise -y
+choco upgrade visualstudio2019buildtools -y
+choco upgrade sql-server-express -y
+choco upgrade vscode -y
+choco upgrade dotnetcore-sdk -y
+choco upgrade azure-cli -y
+choco upgrade microsoftazurestorageexplorer -y
+choco upgrade azure-functions-core-tools -y
+choco upgrade azurepowershell -y
+choco upgrade azure-data-studio -y
+choco upgrade rust -y
+choco upgrade docker-desktop -y
+choco upgrade robo3t -y
+choco upgrade mongodb -y
+choco upgrade filezilla -y
+choco upgrade 7zip -y
+choco upgrade notepadplusplus -y
+choco upgrade audacity -y
+:: Meetings
+choco upgrade skype -y
+choco upgrade microsoft-teams -y
+choco upgrade webex-meetings -y
+choco upgrade zoom -y
+:: Video
+choco upgrade obs-studio -y
 choco upgrade vlc -y
+:: Utilities
 choco upgrade ccleaner -y
 choco upgrade cpu-z -y
 choco upgrade steam -y
-choco upgrade audacity -y
+choco upgrade grammarly -y
+:: Security
+choco upgrade malwarebytes -y
+:: Chocolatey
 choco upgrade chocolatey -y
